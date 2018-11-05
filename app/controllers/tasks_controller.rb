00a8f6288cf70187814e8855b98c279d91273fb2
kosuke <depths.ptk@gmail.com>
2018-11-05 21:35:03 +0900

    add destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,12 @@ class TasksController < ApplicationController
     redirect_to tasks_url, notice: "「#{task.name}」を更新しました。"
   end
 
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "「#{task.name}」を削除しました。"
+  end
+
   def create
     task = Task.new(task_params)
     task.save!

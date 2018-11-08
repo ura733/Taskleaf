b8d7ff14760da77b2b33830ad8e67a4e3e790a2d
kosuke <depths.ptk@gmail.com>
2018-11-08 19:00:26 +0900

    add validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,15 @@
 class Task < ApplicationRecord
-  validates :name, presence: true
-  validates :name, lenght: { maximum: 30}
+  before_validation :set_nameless_name
+  validates :name, presence: true, length: { maximum: 30 }
+  validate :validate_name_not_including_comma
+
+
+  private
+  def set_nameless_name
+    self.name = '名前なし' if name.blank?
+  end
+
+  def validate_name_not_including_comma
+    errors.add(:name, 'にカンマを含める事は出来ません') if name&.include?(',')
+  end
 end

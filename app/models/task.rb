547c261b0f2313c69fa4625cff79769a17edec80
kosuke <depths.ptk@gmail.com>
2018-11-10 00:05:48 +0900

    add login logout admin function

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
+  belongs_to :user
+
 
   private
   def set_nameless_name

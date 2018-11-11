766a85195ed5d3e6dd9a7280524ad57d7ecfb8db
kosuke <depths.ptk@gmail.com>
2018-11-11 19:13:56 +0900

    remove no name task validate

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,4 @@
 class Task < ApplicationRecord
-  before_validation :set_nameless_name
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
 
@@ -9,9 +8,6 @@ class Task < ApplicationRecord
 
 
   private
-  def set_nameless_name
-    self.name = '名前なし' if name.blank?
-  end
 
   def validate_name_not_including_comma
     errors.add(:name, 'にカンマを含める事は出来ません') if name&.include?(',')
--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,26 +1,73 @@
 require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) { FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com') }
+  let(:user_b) { FactoryBot.create(:user, name: 'ユーザB', email: 'b@example.com') }
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
+
+  before do
+    visit login_path
+    fill_in 'seesion_email', with: login_user.email
+    fill_in 'session_password', with: login_user.password
+    click_button 'ログイン'
+  end
+
+  shared_examples_for 'ユーザAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
-    before do
-      # ユーザAを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザA', email: 'a@example.com')
-      # 作成者がユーザAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    context 'ユーザAがログインしている時' do
+      let(:login_user) { user_a }
+
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
     end
 
+    context 'ユーザBがログインしている時' do
+      let(:login_user) { user_b }
+
+      it 'ユーザAが作成したタスクが表示されない' do
+        expect(page).not_to have_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
     context 'ユーザAがログインしている時' do
+      let(:login_user) { user_a }
+
       before do
-        # ユーザAでログインする
-        visit login_path
-        fill_in 'seesion_email', with: 'a@example.com'
-        fill_in 'session_password', with: 'password'
-        click_button 'ログイン'
+        visit task_path(task_a)
       end
 
-      it 'ユーザAが作成したタスクが表示される' do
-        # 作成済みタスクの名称が画面上に表示されている事を確認
-        expect(page).to have_content '最初のタスク'
+      it_behaves_like 'ユーザAが作成したタスクが表示される'
+    end
+  end
+
+  describe '新規作成機能' do
+    let(:login_user) { user_a }
+
+    before do
+      visit new_task_path
+      fill_in 'task_name', with: task_name
+      click_button '登録する'
+    end
+
+    context '新規作成画面で名称を入力した時' do
+      let(:task_name) { '新規作成のテストを書く' }
+
+      it '正常に登録される' do
+        expect(page).to have_selector '.alert-success', text: '新規作成のテストを書く'
+      end
+    end
+
+    context '新規作成画面で名称を入力しなかった時' do
+      let(:task_name) { '' }
+
+      it 'エラーとなる' do
+        within '#error_explanation' do
+          expect(page).to have_content '名称を入力してください'
+        end
       end
     end
   end

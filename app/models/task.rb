cb805b955da1841db7f15e7735639516f6968b29
kosuke <depths.ptk@gmail.com>
2018-11-12 23:51:49 +0900

    add sarch sort fanction

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,6 +6,14 @@ class Task < ApplicationRecord
 
   scope :recent, -> { order(created_at: :desc) }
 
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
 
   private
 
